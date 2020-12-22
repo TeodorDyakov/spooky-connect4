a681135ef62130968bbf754f6bcd567e51c64697
TeodorDyakov <tedy1996@gmail.com>
2020-12-22 23:49:07 +0200

    lines

--- client/go.mod
+++ client/go.mod
@@ -3,6 +3,6 @@ module github.com/yourname/yourgame
 go 1.15
 
 require (
-	github.com/hajimehoshi/ebiten v1.12.5 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.0.2 // indirect
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
 )
--- client/guiGame.go
+++ client/guiGame.go
@@ -6,10 +6,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
 	"image/color"
 	_ "image/png"
+	_ "image/jpeg"
 	"log"
 	"net"
 	"os"
@@ -20,10 +22,14 @@ import (
 var boardImage *ebiten.Image
 var red *ebiten.Image
 var yellow *ebiten.Image
+var lineV *ebiten.Image
+var lineH *ebiten.Image
+var lineU *ebiten.Image
+var lineD *ebiten.Image
 
 func init() {
 	var err error
-	boardImage, _, err = ebitenutil.NewImageFromFile("images/conn4.png")
+	boardImage, _, err = ebitenutil.NewImageFromFile("images/conn4.jpeg")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,6 +41,22 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// lineH, _, err = ebitenutil.NewImageFromFile("images/lineH.png")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// lineV, _, err = ebitenutil.NewImageFromFile("images/lineV.png")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// lineD, _, err = ebitenutil.NewImageFromFile("images/lineD.png")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// lineU, _, err = ebitenutil.NewImageFromFile("images/lineU.png")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
 	if err != nil {
 		log.Fatal(err)
@@ -60,24 +82,25 @@ const (
 	MIN_DIFFICULTY       = 1
 	MAX_DIFFICULTY       = 12
 	SECONDS_TO_MAKE_TURN = 60
-	gravity              = 2
+	fps                  = 60
+	gravity              = 1
 )
 
-var animateCol int
-var fallY float64
+var fallY float64 = 0
 var fallSpeed float64 = 5
 var mplusNormalFont font.Face
 var gameOver bool = false
 var waiting bool = false
 var playerOneWin bool = false
-var gameStarted bool
+var gameStarted bool = false
 var frameCount int = 0
 var lastFrameClicked bool
+
 var mouseClickBuffer chan int = make(chan int)
 var b *Board = NewBoard()
 
 func (g *Game) Update() error {
-	press := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
+	press := inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft)
 
 	if !press && !gameOver && !waiting && lastFrameClicked {
 		mouseX, _ := ebiten.CursorPosition()
@@ -95,7 +118,7 @@ func (g *Game) Update() error {
 }
 
 func col(x int) int {
-	return int(float64(x-tileOffset) / tileHeight)
+	return int(float64(x - tileOffset) / tileHeight)
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -116,12 +139,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	if !gameOver {
 		frameCount++
 	}
-	if frameCount == 3600 {
+	if frameCount == fps * SECONDS_TO_MAKE_TURN {
 		os.Exit(1)
 	}
 	screen.DrawImage(boardImage, nil)
 	text.Draw(screen, msg, mplusNormalFont, 470, 350, color.White)
-	text.Draw(screen, strconv.Itoa(60-frameCount/60)+"s", mplusNormalFont, 470, 380, color.White)
+	text.Draw(screen, strconv.Itoa(SECONDS_TO_MAKE_TURN - frameCount/fps)+"s", mplusNormalFont, 470, 380, color.White)
 
 	for i := 0; i < len(b.board); i++ {
 		for j := 0; j < len(b.board[0]); j++ {
@@ -132,6 +155,23 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			}
 		}
 	}
+	// if(gameOver){
+	// 	con, y, x, lineType := b.whereConnected(PLAYER_ONE_COLOR)
+	// 	if !con{
+	// 		con, y, x, lineType = b.whereConnected(PLAYER_ONE_COLOR)
+	// 	}
+	// 	op := &ebiten.DrawImageOptions{}
+	// 	op.GeoM.Translate(tileOffset+float64(x)*tileHeight + 30, tileOffset+float64(y)*tileHeight+30)
+	// 	if lineType == 0{
+	// 		screen.DrawImage(lineH, op)
+	// 	}else if lineType == 1{
+	// 		screen.DrawImage(lineV, op)
+	// 	}else if lineType == 2{
+	// 		screen.DrawImage(lineD, op)
+	// 	}else if lineType == 3{
+	// 		screen.DrawImage(lineU, op)
+	// 	}
+	// }
 }
 
 var animated [7][6]bool
@@ -162,17 +202,28 @@ func drawTile(x, y int, player string, screen *ebiten.Image) {
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 630, 440
+	return 626, 417
 }
 
 func playAgainstAi() {
+	fmt.Printf("Choose difficulty (number between %d and %d)", MIN_DIFFICULTY, MAX_DIFFICULTY)
+	var option string
+	fmt.Scan(&option)
+
+	difficulty, err := strconv.Atoi(option)
+
+	for err != nil || difficulty < MIN_DIFFICULTY || difficulty > MAX_DIFFICULTY {
+		fmt.Println("Invalid input! Try again:")
+		fmt.Scan(&option)
+		difficulty, err = strconv.Atoi(option)
+	}
 	boardCopy := NewBoard()
 	gameStarted = true
 
 	readyToStartGui <- 1
 	for !b.gameOver() {
 		if waiting {
-			_, bestMove := alphabeta(boardCopy, true, 0, SMALL, BIG, 9)
+			_, bestMove := alphabeta(boardCopy, true, 0, SMALL, BIG, difficulty)
 			b.drop(bestMove, PLAYER_TWO_COLOR)
 			boardCopy.drop(bestMove, PLAYER_TWO_COLOR)
 			time.Sleep(1 * time.Second)
@@ -237,12 +288,12 @@ func playMultiplayer() {
 			column := <-mouseClickBuffer
 			if b.drop(column, color) {
 				frameCount = 0
-				time.Sleep(1 * time.Second)
 				waiting = true
 				_, err := fmt.Fprintf(conn, "%d\n", column)
 				if err != nil {
 					panic(err)
 				}
+				time.Sleep(1 * time.Second)
 			}
 		}
 	}
